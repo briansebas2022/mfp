a9e7f917423c4a51f9547b8e1854e2ac4d3e5c5b
Brian Sebastian <brisebas@in.ibm.com>
2022-08-14 12:10:28 +0530

    added styles provider

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { mount } from 'marketing/MarketingApp'
+import { BrowserRouter } from 'react-router-dom'
 import MarketingAppMount from './components/MarketingAppMount'
+import Header from './components/Header'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
+const generateClassName = createGenerateClassName({ productionPrefix: 'co' })
 
 export default () => {
-   return (
-        <>
-        <h1>Conatiner Ap!!!</h1>
-        <hr></hr>
-        <MarketingAppMount /></>
+    return (
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <>
+                    <Header />
+                    <MarketingAppMount />
+                </>
+            </StylesProvider>
+
+        </BrowserRouter>
+
     )
 }
